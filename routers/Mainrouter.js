9f098bf3f1fbd40eb0cc187539b25a944c838b44
NightGrimalkin <mateusz.mybox@gmail.com>
2023-04-27 22:22:29 +0200

    all functionalities working //TODO add css and errors handling

--- routers/Mainrouter.js
+++ routers/Mainrouter.js
@@ -9,27 +9,55 @@ mainRouter.use("/car", carInfoPageRouter);
 mainRouter.use("/registration", registrationRouter);
 mainRouter.use("/login", loginRouter);
 
-
 const cars = [
-  { name: "Jelcz", torque: 113, power: 82, imageSource: "/images/jelcz_about.jpg"},
-  { name: "Jelcz", torque: 113, power: 82, imageSource: "/images/jelcz_about.jpg"},
-  { name: "Jelcz", torque: 113, power: 82, imageSource: "/images/jelcz_about.jpg"},
+  {
+    name: "Mercedes_S_W116",
+    torque: 226,
+    power: 160,
+  },
+  {
+    name: "Porsche_928",
+    torque: 363,
+    power: 240,
+  },
+  {
+    name: "Renault_R5",
+    torque: 52,
+    power: 34,
+  },
+  {
+    name: "Dodge_Challenger_I ",
+    torque: 461,
+    power: 279,
+  },
+  {
+    name: "Lotus_Esprit",
+    torque: 190,
+    power: 162,
+  },
+  {
+    name: "Volkswagen_Passat_I",
+    torque: 120,
+    power: 72,
+  },
 ];
-
 mainRouter.get("/", (req, res) => {
   const user = {
     userName:
       req.session.userName == ""
         ? "Proszę się zalogować"
         : req.session.userName,
-    last_login:
+    visitsNumber: "",
+    lastLogin:
       req.cookies.last_login == undefined
         ? "Brak ostatniego logowania"
         : new Date(new Date(parseInt(req.cookies.last_login))),
   };
+  let visits = req.cookies.number_of_visits;
   res.render("./pages/layout/mainPage", {
     carData: cars,
     userInfo: user,
+    numberOfVisits: visits,
     pageName: "Strona Główna",
     pageToRender: "main",
   });
--- routers/subrouters/CarInfoPage.js
+++ routers/subrouters/CarInfoPage.js
@@ -1,39 +1,74 @@
 const express = require("express");
+const session = require("express-session");
 const carInfoPageRouter = express.Router();
 
 const cars = [
   {
-    name: "Jelcz",
-    torque: 113,
-    power: 82,
-    imageSource: "/images/jelcz_about.jpg",
+    name: "Mercedes_S_W116",
+    torque: 226,
+    power: 160,
   },
   {
-    name: "Jelcz",
-    torque: 113,
-    power: 82,
-    imageSource: "/images/jelcz_about.jpg",
+    name: "Porsche_928",
+    torque: 363,
+    power: 240,
   },
   {
-    name: "Jelcz",
-    torque: 113,
-    power: 82,
-    imageSource: "/images/jelcz_about.jpg",
+    name: "Renault_R5",
+    torque: 52,
+    power: 34,
+  },
+  {
+    name: "Dodge_Challenger_I ",
+    torque: 461,
+    power: 279,
+  },
+  {
+    name: "Lotus_Esprit",
+    torque: 190,
+    power: 162,
+  },
+  {
+    name: "Volkswagen_Passat_I",
+    torque: 120,
+    power: 72,
   },
 ];
 
 carInfoPageRouter.get("/:page", (req, res) => {
   const chosenCarName = req.params.page;
   let chosenCar;
-  cars.forEach((car) =>{
-    if(car.name==chosenCarName){
-        chosenCar=car;
-        chosenCar.imageSource=chosenCarName+".jpg";
-        console.log(chosenCar);
+  cars.forEach((car) => {
+    if (car.name == chosenCarName) {
+      chosenCar = car;
+      chosenCar.imageSource = chosenCarName + ".jpg";
     }
-  })
-  res.render('./pages/layout/chosenCar',{car: chosenCar})
+  });
+  const user = {
+    userName: req.session.userName,
+    visitsNumber: req.cookies.number_of_visits[chosenCarName],
+  };
+  if (req.session.logged_in == true) {
+    if (chosenCar != undefined) {
+      let numberOfVisits = req.cookies.number_of_visits;
+      if (numberOfVisits != undefined) {
+        numberOfVisits[chosenCarName] = numberOfVisits[chosenCarName] + 1;
+      } else {
+        numberOfVisits = {};
+        numberOfVisits[chosenCarName] = 1;
+      }
+      res.cookie("number_of_visits", numberOfVisits, { maxAge: 900000 });
+      res.render("./pages/layout/chosenCar", {
+        car: chosenCar,
+        userInfo: user,
+        pageName: chosenCarName,
+      });
+    } else {
+      res.send("<h1>Strona nie istnieje</h1>");
+    }
+  } else {
+    res.redirect("/login");
+  }
 });
-
 
 module.exports = carInfoPageRouter;
